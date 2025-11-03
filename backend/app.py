20b90fe7a36e68edd9ff844b9da2bb6a77d1d952
Luis Rodriguez <luiisrodriguezguevara@gmail.com>
2025-11-03 01:27:19 -0500

    Remove app.run from backend/app.py
    Remove the main entry point for the application.

--- backend/app.py
+++ backend/app.py
@@ -91,6 +91,3 @@ def delete_task(id):
     return jsonify({"message": "Deleted successfully"})
 
 # --------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    app.run(debug=True)
